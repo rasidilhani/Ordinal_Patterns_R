2071e224094c4a8b91301a5c2069c9fa6f5f223e
rasidilhani <133581239+rasidilhani@users.noreply.github.com>
2024-08-27 07:39:31 +1200

    Fault analysis basic functionality added.

---
+++ Code/R/discriminate_faulty_ci.R
@@ -0,0 +1,146 @@
+# Required libraries
+library(rprojroot)
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
+theme_set(theme_clean()+theme(legend.position = "top"))
+library(StatOrdPattHxC)
+
+data("LinfLsup")
+
+# Define the base path of the project using rprojroot
+base_path <- find_root(has_file("README.md"))
+
+# Build the relative path to the Excel file
+file_path <- file.path(base_path, "Data", "csv", "Normal_baseline_data.csv")
+plot_path <- file.path(base_path, "Plots")
+
+if(!dir.exists(plot_path)) {
+  dir.create(plot_path)
+}
+
+directory_path_fault_analysis = file.path(plot_path, "fault_analysis")
+
+if(!dir.exists(directory_path_fault_analysis)) {
+  dir.create(directory_path_fault_analysis)
+}
+
+read_data_file <- function(file_path){
+  # Check if the file exists
+  if (!file.exists(file_path)) {
+    stop("The file does not exist: ", file_path)
+  }
+  
+  # Read the CSV file
+  data_normal <- read_csv(file_path, show_col_types = FALSE)
+  
+  # Select specific columns for analysis
+  selected_columns <- data_normal %>% 
+    select(Fault_diameter, Motor_load, RPM, DE_time, FE_time, BA_time)
+  
+  # Example: Print the first few rows of the selected columns
+  #print(head(selected_columns))
+  return (selected_columns)
+  
+}
+
+df <- read_data_file(file_path)
+
+df_keys <- list("DE_time", "FE_time", "BA_time")
+plot_colors <- list("green", "orange", "purple")
+#df_keys <- list("DE_time")
+
+head(df)
+
+for(ML in 0:3){
+  for(D in 3:6){
+    de_time_data <- df[df$Motor_load == ML, ]$DE_time
+    fe_time_data <- df[df$Motor_load == ML, ]$FE_time
+    ba_time_data <- df[df$Motor_load == ML, ]$BA_time
+    
+    # Matrix that stores the variances
+    Variances <- matrix(nrow=2, ncol=1)
+    
+    if(sum(!is.na(ba_time_data)) > 0) {
+      Variances <- matrix(nrow=3, ncol=1)
+    }
+    
+    chunk_limit = length(de_time_data) * 0.1 # 10% from the data length
+    
+    Variances[1,1] <- sigma2q(de_time_data[0:chunk_limit], emb = D, ent = "S")
+    Variances[2,1] <- sigma2q(fe_time_data[0:chunk_limit], emb = D, ent = "S")
+    
+    if(sum(!is.na(ba_time_data)) > 0) {
+      Variances[3,1] <- sigma2q(ba_time_data[0:chunk_limit], emb = D, ent = "S")
+    }
+    
+    Variances[Variances<0] <- 0
+    
+    x1sub <- de_time_data
+    x2sub <- fe_time_data
+    x3sub <- ba_time_data
+    
+    if(sum(!is.na(x3sub)) > 0) {
+      ShannonEntropies <- c(
+        HShannon(OPprob(x1sub, emb=D)),
+        HShannon(OPprob(x2sub, emb=D)),
+        HShannon(OPprob(x3sub, emb=D))
+      )
+    }else{
+      ShannonEntropies <- c(
+        HShannon(OPprob(x1sub, emb=D)),
+        HShannon(OPprob(x2sub, emb=D))) 
+    }
+    
+    
+    if(sum(!is.na(x3sub)) > 0) {
+      StatisticalComplexities <- c(
+        StatComplexity(OPprob(x1sub, emb=D)),
+        StatComplexity(OPprob(x2sub, emb=D)),
+        StatComplexity(OPprob(x3sub, emb=D))
+      )
+    }else{
+      StatisticalComplexities <- c(
+        StatComplexity(OPprob(x1sub, emb=D)),
+        StatComplexity(OPprob(x2sub, emb=D))
+      )  
+    }
+    
+    alpha <- 0.05
+    StandardDeviations <- sqrt(Variances[,1])
+    SemiLength <- StandardDeviations/sqrt(length(x1sub)-D)*qnorm(1-alpha/2) 
+    # The three time series have the same length, but they could be different
+    
+    features_c <- c("DE time", "FE time")
+    
+    if(sum(!is.na(ba_time_data)) > 0) {
+      features_c <- c("DE time", "FE time", "BA_time")
+    }
+    
+    HCPoints <- data.frame(H=ShannonEntropies,
+                           C=StatisticalComplexities,
+                           STD=StandardDeviations,
+                           SemiLength=SemiLength,
+                           Series=as.factor(features_c))
+    
+    pdf_file_name <- file.path(directory_path_fault_analysis, paste(sprintf("confidence_interval_Embed_dim_%s_Motor_load_%s", D, ML), ".pdf"))
+    
+    ggplot(subset(LinfLsup, Side=="Lower" & Dimension==as.character(D)), 
+           aes(x=H, y=C)) +
+      geom_line() +
+      geom_line(data=subset(LinfLsup, Side=="Upper" & Dimension==as.character(D)), 
+                aes(x=H, y=C)) +
+      xlab(expression(italic(H))) +
+      ylab(expression(italic(C))) +
+      geom_point(data=HCPoints, aes(x=H, y=C, col=Series)) +
+      geom_errorbarh(data=HCPoints, aes(xmin=H-SemiLength, xmax=H+SemiLength, group=Series, col=Series)) +
+      coord_cartesian(xlim=c(0.65, 1), ylim=c(0, 0.4))
+    
+    ggsave(pdf_file_name, width=16, height = 10, units = "in", dpi = 300)
+  }
+}
+
+
+
