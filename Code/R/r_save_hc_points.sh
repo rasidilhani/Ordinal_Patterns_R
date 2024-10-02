c09a64b4f9eb67b965af5ac1094851e50c5792a4
rasidilhani <133581239+rasidilhani@users.noreply.github.com>
2024-10-02 21:15:41 +1300

    Prepare for HPC

---
+++ Code/R/r_save_hc_points.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=save_hc_points
+#SBATCH -o save_hc_points.out
+#SBATCH -e save_hc_points.err
+#
+#SBATCH --cpus-per-task=4 #Note: you are always allocated an even number of cpus
+#SBATCH --mem=16G
+#SBATCH --time=10:00
+
+module purge
+module load GCC/11.2.0 OpenMPI/4.1.1
+module load R/4.2.0   
+
+Rscript HPC_save_hc_points.R
