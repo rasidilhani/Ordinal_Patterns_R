92b0450760a3cec6bc148c6fdd57394b2b676af3
rasidilhani <133581239+rasidilhani@users.noreply.github.com>
2024-10-03 16:30:00 +1300

    HPC error diagnosis 10

--- Code/R/HPC_save_hc_points.R
+++ Code/R/HPC_save_hc_points.R
@@ -75,5 +75,79 @@ plot_colors <- list("green", "orange", "purple")
 #df = df[rowSums(is.na(df)) == 0, ] 
 
 head(df)
-
+for(ML in 0:3){
+  for(D in 5:6){
+    plot_title = sprintf("Confidence intervel - Motor load: %s, Embed dim: %s", ML, D)
+    
+    de_time_data <- df[df$Motor_load == ML, ]$DE_time
+    fe_time_data <- df[df$Motor_load == ML, ]$FE_time
+    ba_time_data <- df[df$Motor_load == ML, ]$BA_time
+    
+    # Matrix that stores the variances
+    Variances <- matrix(nrow=2, ncol=1)
+    
+    if(sum(!is.na(ba_time_data)) > 0) {
+      Variances <- matrix(nrow=3, ncol=1)
+    }
+    
+    #chunk_limit = length(de_time_data) * 0.1 # 10% from the data length
+    
+    Variances[1,1] <- sigma2q(de_time_data, emb = D, ent = "S")
+    Variances[2,1] <- sigma2q(fe_time_data, emb = D, ent = "S")
+    
+    if(sum(!is.na(ba_time_data)) > 0) {
+      Variances[3,1] <- sigma2q(ba_time_data, emb = D, ent = "S")
+    }
+    
+    Variances[Variances<0] <- 0
+    
+    x1sub <- de_time_data
+    x2sub <- fe_time_data
+    x3sub <- ba_time_data
+    
+    if(sum(!is.na(x3sub)) > 0) {
+      ShannonEntropies <- c(
+        HShannon(OPprob(x1sub, emb=D)),
+        HShannon(OPprob(x2sub, emb=D)),
+        HShannon(OPprob(x3sub, emb=D))
+      )
+      
+      StatisticalComplexities <- c(
+        StatComplexity(OPprob(x1sub, emb=D)),
+        StatComplexity(OPprob(x2sub, emb=D)),
+        StatComplexity(OPprob(x3sub, emb=D))
+      )
+    }else{
+      ShannonEntropies <- c(
+        HShannon(OPprob(x1sub, emb=D)),
+        HShannon(OPprob(x2sub, emb=D))
+      ) 
+      
+      StatisticalComplexities <- c(
+        StatComplexity(OPprob(x1sub, emb=D)),
+        StatComplexity(OPprob(x2sub, emb=D))
+      )
+    }
+    
+    alpha <- 0.05
+    StandardDeviations <- sqrt(Variances[,1])
+    SemiLength <- StandardDeviations/sqrt(length(x1sub)-D)*qnorm(1-alpha/2) 
+    # The three time series have the same length, but they could be different
+    
+    features_c <- c("DE time", "FE time")
+    
+    if(sum(!is.na(ba_time_data)) > 0) {
+      features_c <- c("DE time", "FE time", "BA_time")
+    }
+    
+    HCPoints <- data.frame(H=ShannonEntropies,
+                           C=StatisticalComplexities,
+                           STD=StandardDeviations,
+                           SemiLength=SemiLength,
+                           Series=as.factor(features_c))
+    
+    write.csv(HCPoints, file.path(base_path, "Data", "csv", sprintf("HCPoints_%s_ML_%s_D_%s.csv", dataset_type, ML, D)))
+    
+  }
+}
 #print("done!!")
