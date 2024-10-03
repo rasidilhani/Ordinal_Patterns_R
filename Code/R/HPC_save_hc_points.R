43e500344c1702abdaaeb22c368e4841543e8445
rasidilhani <133581239+rasidilhani@users.noreply.github.com>
2024-10-03 15:52:03 +1300

    HPC error diagnose 3

--- Code/R/HPC_save_hc_points.R
+++ Code/R/HPC_save_hc_points.R
@@ -25,4 +25,55 @@ if(!dir.exists(data_path)) {
   dir.create(data_path, recursive = TRUE)
 }
 
-print("done!")
+#### uncomment following lines to change the data file #######
+dataset_type = "Normal_baseline"
+#dataset_type = "12k_drive_end_bearing_fault"
+#dataset_type = "12kFan_end_bearing_fault"
+#dataset_type = "48k_drive_end_bearing_fault"
+
+# Build the relative path to the Excel file
+file_path <- file.path(base_path, "Data", "csv", sprintf("%s_data.csv", dataset_type))
+
+directory_path_fault_analysis = file.path(plot_path, "fault_analysis")
+
+if(!dir.exists(directory_path_fault_analysis)) {
+  dir.create(directory_path_fault_analysis)
+}
+
+
+read_data_file <- function(file_path){
+  # Check if the file exists
+  if (!file.exists(file_path)) {
+    stop("The file does not exist: ", file_path)
+  }
+  
+  # Read the CSV file
+  data_normal <- read_csv(file_path, show_col_types = FALSE)
+  
+  # Select specific columns for analysis
+  selected_columns <- data_normal %>% 
+    select(Fault_diameter, Motor_load, RPM, DE_time, FE_time, BA_time)
+  
+  # Example: Print the first few rows of the selected columns
+  #print(head(selected_columns))
+  return (selected_columns)
+  
+}
+
+df <- read_data_file(file_path)
+
+df_keys <- list("DE_time", "FE_time", "BA_time")
+plot_colors <- list("green", "orange", "purple")
+#df_keys <- list("DE_time")
+
+# check for NA values for different Fields
+sum(is.na(df$DE_time))
+sum(is.na(df$FE_time))
+sum(is.na(df$BA_time))
+
+# remove na values
+#df = df[rowSums(is.na(df)) == 0, ] 
+
+head(df)
+
+print("done!!")
