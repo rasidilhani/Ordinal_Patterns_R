7e027e47d99febba6e4b545248e01be39585ea7d
rasidilhani <133581239+rasidilhani@users.noreply.github.com>
2024-11-24 11:07:20 +1300

    updated aggregate function

--- Code/R/agregate_function_12kDrive.R
+++ Code/R/agregate_function_12kDrive.R
@@ -1,19 +1,11 @@
 data <- X12k_drive_end_bearing_fault_data
 
-Drive_S_FE_5 <-aggregate(data,
-                    FE_time~ Motor_load,
-                    FUN = sigma2q, emb=5) 
-
-Drive_S_FE_6 <-aggregate(data,
-                   FE_time~ Motor_load,
-                   FUN = sigma2q, emb=6) 
-
-Drive_S_BE_5 <-aggregate(data,
-                   BE_time~ Motor_load,
+Drive_S_DE_5 <-aggregate(data,
+                   DE_time~ Motor_load,
                    FUN = sigma2q, emb=5) 
 
-Drive_S_BE_6 <-aggregate(data,
-                   BE_time~ Motor_load,
+Drive_S_DE_6 <-aggregate(data,
+                   DE_time~ Motor_load,
                    FUN = sigma2q, emb=6) 
 
 Drive_S_BA_5 <-aggregate(data,
