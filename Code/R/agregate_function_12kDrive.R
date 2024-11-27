dfe2ff59d7735e3c4ca29adf362e0cab5c4acc50
Alejandro C. Frery <acfrery@gmail.com>
2024-11-28 11:19:56 +1300

    Update agregate_function_12kDrive.R

--- Code/R/agregate_function_12kDrive.R
+++ Code/R/agregate_function_12kDrive.R
@@ -1,4 +1,6 @@
-data <- X12k_drive_end_bearing_fault_data
+library("StatOrdPattHxC")
+
+data <- read_csv("~/Library/CloudStorage/OneDrive-VictoriaUniversityofWellington-STAFF/Documents/Alunos/Rasika Dilhani/Ordinal_Patterns_R/Data/csv/12k_drive_end_bearing_fault_data.csv")
 
 Drive_S_DE_5 <-aggregate(data,
                    DE_time~ Motor_load,
@@ -15,3 +17,5 @@ Drive_S_BA_5 <-aggregate(data,
 Drive_S_BA_6 <-aggregate(data,
                    BA_time~ Motor_load,
                    FUN = sigma2q, emb=6)
+
+save(file="../Results/Alejandro3.RData", Drive_S_BA_6)
