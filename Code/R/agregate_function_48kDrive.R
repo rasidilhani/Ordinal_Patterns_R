d3bcb891242227614fdbeaaf56083d96ca7bb56a
Alejandro C. Frery <acfrery@gmail.com>
2024-12-06 10:44:02 +1300

    Update agregate_function_48kDrive.R

--- Code/R/agregate_function_48kDrive.R
+++ Code/R/agregate_function_48kDrive.R
@@ -1,14 +1,18 @@
-data <- read_csv("~/Library/CloudStorage/OneDrive-VictoriaUniversityofWellington-STAFF/Documents/Alunos/Rasika Dilhani/Ordinal_Patterns_R/Data/csv/48k_drive_end_bearing_fault_data.csv")
+library(readr)
+library(StatOrdPattHxC)
+
+data <- read_csv("../../Data/csv/48k_drive_end_bearing_fault_data.csv")
+
 
 Drive48_S_DE_6 <-aggregate(data,
                          DE_time~ Motor_load,
                          FUN = sigma2q, emb=6) 
-save(file="../Results/Drive48_S_DE_6.RData", Drive_S_DE_6)
+save(file="../../Data/Results/Drive48_S_DE_6.RData", Drive_S_DE_6)
 
 Drive48_S_FE_6 <-aggregate(data,
                          FE_time~ Motor_load,
                          FUN = sigma2q, emb=6) 
-save(file="../Results/Drive48_S_FE_6.RData", Drive_S_DE_6)
+save(file="../../Data/Results/Drive48_S_FE_6.RData", Drive_S_DE_6)
 
 
                                                                                          
