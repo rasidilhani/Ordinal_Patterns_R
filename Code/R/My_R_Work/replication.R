27d7b786dacb346c0e743f399576728707a00988
Andrea Rey <63259737+arey1911@users.noreply.github.com>
2025-09-17 08:52:00 -0300

    Sketch for the ARIMA experiments

--- Code/R/My_R_Work/replication.R
+++ Code/R/My_R_Work/replication.R
@@ -1,47 +1,20 @@
 library(StatOrdPattHxC)
 library(ggplot2)
-
-D <- 3
-n <- 1000
-
-ts_data <- arima.sim(model = list(ar1 = 0.8, ar2 = 0.5, ma1 = 0.8, ma2 = 0.5), n = n)
-ProbARMA <- OPprob(ts_data, emb = D)
-Entropy <- HShannon(ProbARMA)
-Complexity <- StatComplexity(ProbARMA)
-
-
-#####################################################################################################
-library(StatOrdPattHxC)
-D <- 3
-n <- 100
-
-# Function for a single run
-arma_entropy_complexity <- function() {
-  ts_data <- arima.sim(model = list(ar1 = 0.8, ar2 = 0.5, ma1 = 0.8, ma2 = 0.5), n = n)
-  ProbARMA <- OPprob(ts_data, emb = D)
-  H <- HShannon(ProbARMA)
-  C <- StatComplexity(ProbARMA)
-  c(Entropy = H, Complexity = C)
-}
-
-set.seed(123)  # for reproducibility!
-results_matrix <- replicate(300, arma_entropy_complexity())
-
-# Convert to a data frame for easy use (transpose if needed)
-results_df <- data.frame(t(results_matrix))
-head(results_df)
-
-=======
+library(ggExtra)
 library(ggthemes)
 library(GGally)
+
+data("LinfLsup")
+
+###############################################################################
 
 set.seed(123456789, kind="Mersenne-Twister")
 
 D <- 3
-N <- c(100, 300, 500, 1000)
+N <- c(500, 1000)
 R <- 100
 
-data("LinfLsup")
+### Sketch of the experiment (one example)
 
 ### ARMA(2,2)
 
@@ -49,33 +22,8 @@ Output <- NULL
 
 for(n in N){
   for(r in 1:R){
-  
-    ts_data <- arima.sim(model = list(ar1 = 0.8, ar2 = 0.5, ma1 = 0.8, ma2 = 0.5), n = n)
-    ProbARMA <- OPprob(ts_data, emb = D)
-    Entropy <- HShannon(ProbARMA)
-    Complexity <- StatComplexity(ProbARMA)
-  
-    Output <- rbind(Output, c(Entropy, Complexity, n))
-  }
-}
-
-Output <- data.frame(Output)
-names(Output) <- c("Entropy", "Complexity", "n")
-  
-Output$n <- as.factor(Output$n)
-Output.molten <- reshape2::melt(Output)
-
-ggplot(Output, aes(x=Entropy, y=Complexity, col=n)) +
-  geom_point() 
-
-### ARMA(1,1)
-
-Output <- NULL
-
-for(n in N){
-  for(r in 1:R){
     
-    ts_data <- arima.sim(model = list(ar1 = 0.8, ma1 = 0.8), n = n)
+    ts_data <- arima.sim(model = list(ar1 = 0.1, ar2 = 0.8, ma1 = 0.1, ma2 = 0.8), n = n)
     ProbARMA <- OPprob(ts_data, emb = D)
     Entropy <- HShannon(ProbARMA)
     Complexity <- StatComplexity(ProbARMA)
@@ -90,60 +38,21 @@ names(Output) <- c("Entropy", "Complexity", "n")
 Output$n <- as.factor(Output$n)
 Output.molten <- reshape2::melt(Output)
 
-ggplot(subset(Output, n=="500" | n=="1000"), aes(x=Entropy, y=Complexity, col=n)) +
-  geom_point() +
-  theme_pander()  
-
-### AR(2)
-
-Output <- NULL
-
-for(n in N){
-  for(r in 1:R){
-    
-    ts_data <- arima.sim(model = list(ar1 = -0.8, ar2=-.15), n = n)
-    ProbARMA <- OPprob(ts_data, emb = D)
-    Entropy <- HShannon(ProbARMA)
-    Complexity <- StatComplexity(ProbARMA)
-    
-    Output <- rbind(Output, c(Entropy, Complexity, n))
-  }
-}
-
-Output <- data.frame(Output)
-names(Output) <- c("Entropy", "Complexity", "n")
-
-Output$n <- as.factor(Output$n)
-Output.molten <- reshape2::melt(Output)
-
-ggplot() +
+p <- ggplot() +
   geom_line(data = subset(LinfLsup, Side == "Lower" & Dimension == "3"), 
             aes(x = H, y = C), col="lightgray") +
   geom_line(data = subset(LinfLsup, Side == "Upper" & Dimension == "3"), 
             aes(x = H, y = C), col="lightgray") +
-  geom_jitter(data = subset(Output, n=="500" | n=="1000"), 
-              aes(x=Entropy, y=Complexity, col=n),
-              width=0, height=.0005
-                ) +
+  geom_point(data = subset(Output, n=="500" | n=="1000"), 
+             aes(x=Entropy, y=Complexity, col=n)
+  ) +
   xlim(.99, 1) +
+  xlab(expression(italic(H))) +
   ylim(0, 0.01) +
-  theme_pander()  
+  ylab(expression(italic(C))) +
+  theme_pander() +
+  theme(legend.position = "bottom")
 
-ggMarginal(data = subset(Output, n=="500" | n=="1000"),
-  ggplot() +
-    geom_line(data = subset(LinfLsup, Side == "Lower" & Dimension == "3"), 
-              aes(x = H, y = C), col="lightgray") +
-    geom_line(data = subset(LinfLsup, Side == "Upper" & Dimension == "3"), 
-              aes(x = H, y = C), col="lightgray") +
-    geom_point(data = subset(Output, n=="500" | n=="1000"), 
-                aes(x=Entropy, y=Complexity, col=n)
-    ) +
-    xlim(.99, 1) +
-    xlab(expression(italic(H))) +
-    ylim(0, 0.01) +
-    ylab(expression(italic(C))) +
-    theme_pander() +
-    theme(legend.position = "bottom"),
-  groupColour=TRUE
-)
+ggMarginal(p, groupColour=TRUE)
+
 
